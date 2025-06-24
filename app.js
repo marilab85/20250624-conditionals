d5c0373fa7d532368ccea82df4ffd243c74678c1
Marina Labonia <marina.mlabonia@gmail.com>
2025-06-24 13:26:45 +0100

    Commit the stretch

---
+++ app.js
@@ -0,0 +1,10 @@
+let age = 17;
+let legalDrivingAge = 17;
+
+if (age > legalDrivingAge) {
+  console.log("You can drive!");
+} else if (age === legalDrivingAge) {
+  console.log("You can, but should you?");
+} else {
+  console.log("oooops, can't drive :(");
+}
---
+++ labonia.js
@@ -0,0 +1,8 @@
+let lookLike = 30;
+let realAge = 40;
+
+if (lookLike > realAge) {
+  console.log("you are not doing it right");
+} else if (realAge < 50) {
+  console.log("You are getting there");
+}
